d079f9deef0b7c0bc00d45d1d0f66c0c530b6a5d
Zam <samuel.kodehode@gmail.com>
2024-01-16 10:57:28 +0100

    1.0.0

---
+++ script.js
@@ -0,0 +1,3 @@
+for (var i = 0; i <= 100; i++) {
+    console.log(!(i % 3 + i % 5) ? 'FizzBuzz' : !(i % 3) ? 'Fizz' : !(i % 5) ? 'Buzz' : i);
+}
---
+++ script.ts
@@ -0,0 +1,7 @@
+
+
+for (let i: number = 0; i <= 100; i++){console.log(!(i % 3 + i % 5) ? 'FizzBuzz' : !(i % 3) ? 'Fizz' : !(i % 5) ? 'Buzz' : i)}
+
+
+
+
